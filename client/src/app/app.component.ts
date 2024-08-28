196de45c8477b7c94ca5fb961516018117461e22
Ant <Kozakanton@i.ua>
2024-08-28 12:37:11 +0200

    added filters and sortering

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,30 +1,15 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ShopComponent } from './features/shop/shop.component';
 import { HeaderComponent } from './layout/header/header.component';
-import { Pagination } from './shared/Models/Pagination';
-import { Product } from './shared/Models/Products';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/';
-  private http = inject(HttpClient);
-
+export class AppComponent {
   title = "Antonio's Angular App";
-
-  products: Product[] = [];
-
-  ngOnInit(): void {
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: (response) => (this.products = response.data),
-      error: (error) => console.error,
-      complete: () => console.log('complete'),
-    });
-  }
 }
